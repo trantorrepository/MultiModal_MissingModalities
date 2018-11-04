bc66110e73d02c422aa8d7afb10109f27739a529
Raphael <raphael.couronne@gmail.com>
2018-11-04 14:52:09 +0100

    mnist example update

--- experiments/MNIST/mnist.py
+++ experiments/MNIST/mnist.py
@@ -37,7 +37,7 @@ def multimodal_transformation(input):
 
 # Hyper Parameters
 EPOCH = 10
-BATCH_SIZE = 3
+BATCH_SIZE = 20
 DOWNLOAD_MNIST = True
 
 
@@ -90,14 +90,14 @@ class Image_Decoder(nn.Module):
         super(Image_Decoder, self).__init__()
 
         # Encoder
-        self.fc = nn.Linear(dim_latent, 32)
-        self.deconv3 = nn.ConvTranspose2d(in_channels=8,out_channels=9,kernel_size=3,stride=2)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=9,out_channels=12,kernel_size=5,stride=3, padding = 1)
-        self.deconv1 = nn.ConvTranspose2d(in_channels=12,out_channels=1,kernel_size=2,stride=2, padding = 1)
+        self.fc = nn.Linear(dim_latent,4*4*8)
+        self.deconv3 = nn.ConvTranspose2d(in_channels=8,out_channels=32,kernel_size=2,stride=2)
+        self.deconv2 = nn.ConvTranspose2d(in_channels=32,out_channels=16,kernel_size=4,stride=2, padding = 1)
+        self.deconv1 = nn.ConvTranspose2d(in_channels=16,out_channels=1, kernel_size=2, stride=2, padding = 2)
 
     def forward(self, x):
         x = self.fc(x)
-        x = x.view(-1,8,2,2)
+        x = x.view(-1,8,4,4)
         x = F.relu(self.deconv3(x)) # torch.Size([32, 9, 5, 5])
         x = F.relu(self.deconv2(x)) # torch.Size([32, 12, 14, 14])
         x = F.sigmoid(self.deconv1(x)) # torch.Size([32, 1, 28, 28])
@@ -222,81 +222,6 @@ multi_autoencoder.forward([modalities[0],
 
 
 
-# Create the NN
-batch_size = BATCH_SIZE
-dim_latent = 10
-
-# Create the Auto encoder
-encoder_1 = Image_Encoder(dim_latent)
-encoder_2 = Image_Encoder(dim_latent)
-encoder_3 = Number_Encoder(dim_input, dim_latent)
-
-decoder_1 = Image_Decoder(dim_latent)
-decoder_2 = Image_Decoder(dim_latent)
-decoder_3 = Number_Decoder(dim_input, dim_latent)
-
-encoders = [encoder_1, encoder_2, encoder_3]
-decoders = [decoder_1, decoder_2, decoder_3]
-
-multi_autoencoder = Autoencoder(encoders, decoders,  dim_latent,  batch_size)
-
-
-## Launch parameters
-lr = 0.001
-criterion_img = nn.MSELoss()
-criterion_number = nn.CrossEntropyLoss()
-optimizers_encoders = [optim.Adam(encoder.parameters(), lr=lr) for encoder in multi_autoencoder.encoders]
-optimizers_decoders = [optim.Adam(decoder.parameters(), lr=lr) for decoder in multi_autoencoder.decoders]
-optimizers = optimizers_encoders+optimizers_decoders
-
-n_epoch = 10
-
-start = time.time()
-
-
-for epoch in range(n_epoch):
-    for i, (img_modalities, label) in enumerate(train_loader):
-
-        # Add the label
-        label_onehot = torch.zeros(batch_size, dim_input)
-        label_onehot = label_onehot.scatter(1, label.view(-1, 1), 1)
-
-        modalities = img_modalities+[torch.FloatTensor(label_onehot).view(batch_size, 1, dim_input)]
-
-        #indices = [1,2]
-        indices = np.unique(np.random.choice((1, 2), 2))
-
-        # Randomly remove modalities
-
-        # Initialize hidden, grad, loss
-        multi_autoencoder.zero_grad()
-        loss = 0
-
-        output = multi_autoencoder(modalities, indices)
-        losses = [criterion_img(output[0], modalities[0]),
-               criterion_img(output[1], modalities[1]),
-               criterion_number(output[2].view(batch_size, dim_input), label)]
-
-        for idx in indices:
-            loss+=losses[idx]
-
-
-        # Gradient step
-        loss.backward()
-        for optimizer in optimizers:
-            optimizer.step()
-
-        if i>500:
-            break
-
-    plot_reconstruction(multi_autoencoder, (img_modalities, label),
-                        indices, epoch, batch_limit=3)
-
-    print("Epoch {0}, {1} : {2}".format(epoch, timeSince(start), loss.data.view(-1).numpy()[0]))
-
-#%% Test the results
-
-
 def plot_reconstruction(multi_autoencoder, sample,
                         indices, batch_size, epoch=0, batch_limit=1, plot=False):
 
@@ -329,9 +254,84 @@ def plot_reconstruction(multi_autoencoder, sample,
         axes[2*batch+1, 2].bar(range(0, 10),output[2][batch, 0, :].data.numpy())
 
     plt.tight_layout()
-    plt.savefig('./experiments/MNIST/output/mnist_epoch{0}.pdf'.format(epoch))
+    plt.savefig('/Users/raphael.couronne/Programming/ARAMIS/Projects/MultiModal_MissingModalities/experiments/MNIST/output/mnist_epoch{0}.pdf'.format(epoch))
     if plot:
         plt.show()
+
+
+# Create the NN
+batch_size = BATCH_SIZE
+dim_latent = 14
+
+# Create the Auto encoder
+encoder_1 = Image_Encoder(dim_latent)
+encoder_2 = Image_Encoder(dim_latent)
+encoder_3 = Number_Encoder(dim_input, dim_latent)
+
+decoder_1 = Image_Decoder(dim_latent)
+decoder_2 = Image_Decoder(dim_latent)
+decoder_3 = Number_Decoder(dim_input, dim_latent)
+
+encoders = [encoder_1, encoder_2, encoder_3]
+decoders = [decoder_1, decoder_2, decoder_3]
+
+multi_autoencoder = Autoencoder(encoders, decoders,  dim_latent,  batch_size)
+
+
+## Launch parameters
+lr = 0.0005
+criterion_img = nn.MSELoss()
+criterion_number = nn.CrossEntropyLoss()
+optimizers_encoders = [optim.Adam(encoder.parameters(), lr=lr) for encoder in multi_autoencoder.encoders]
+optimizers_decoders = [optim.Adam(decoder.parameters(), lr=lr) for decoder in multi_autoencoder.decoders]
+optimizers = optimizers_encoders+optimizers_decoders
+
+n_epoch = 10
+
+start = time.time()
+
+
+for epoch in range(n_epoch):
+    for i, (img_modalities, label) in enumerate(train_loader):
+
+        # Add the label
+        label_onehot = torch.zeros(batch_size, dim_input)
+        label_onehot = label_onehot.scatter(1, label.view(-1, 1), 1)
+
+        modalities = img_modalities+[torch.FloatTensor(label_onehot).view(batch_size, 1, dim_input)]
+
+        indices = [0,1]
+        #indices = np.unique(np.random.choice((0,1, 2), 3))
+
+        # Randomly remove modalities
+
+        # Initialize hidden, grad, loss
+        multi_autoencoder.zero_grad()
+        loss = 0
+
+        output = multi_autoencoder(modalities, indices)
+        losses = [criterion_img(output[0], modalities[0]),
+               criterion_img(output[1], modalities[1]),
+               criterion_number(output[2].view(batch_size, dim_input), label)]
+
+        for idx in indices:
+            loss+=losses[idx]
+
+
+        # Gradient step
+        loss.backward()
+        for optimizer in optimizers:
+            optimizer.step()
+
+        if i>500:
+            break
+
+    plot_reconstruction(multi_autoencoder, (img_modalities, label),
+                        indices, batch_size, epoch, batch_limit=3)
+
+    print("Epoch {0}, {1} : {2}".format(epoch, timeSince(start), loss.data.view(-1).numpy()[0]))
+
+
 
 
 #%% Test the plot
@@ -342,5 +342,5 @@ for i, sample in enumerate(train_loader):
 
 
 indices = [2]
-plot_reconstruction(multi_autoencoder, sample, indices,batch_size, batch_limit=3, plot=True)
+plot_reconstruction(multi_autoencoder, sample, indices, batch_size, batch_limit=3, plot=True)
 
